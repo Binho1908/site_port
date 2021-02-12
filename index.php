8fab3bb8b73475eb19d614aa5c72974c234a3449
binho1908 <fabiosenninhaddt@gmail.com>
2021-02-11 22:29:08 -0300

    adicionados 37 a 40 cores na pasta assts/css, atualizado o arquivo principal index.php onde ele estava com uma tag center, a tag foi removida e o problema foi resolvido com uma estilização no head da pagina trazendo uma tag style e seus valores para resolver tal problema.

--- index.php
+++ index.php
@@ -8,189 +8,179 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/estilo.css">
     <title>Curso php</title>
+    <style>
+        li {
+            display: block;
+        }
+        p, a, img {
+            text-align: center;
+            justify-content: center;
+        }
+    </style>
 </head>
 <body>
     <header class="cabecalho">
         <h1>Projetos</h1>
         <h2>Estudos 2021</h2>
     </header>
-    
     <main class="principal">
         <div class="conteudo">
             <nav class="modulos">
-                <div class="modulo laranja">
+                <div class="modulo cinquentaedois">
                     <h3>Site de Hotel</h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Hotel Paraiso</p>
                             <a href="hotel_paraiso/index.php">
                             <img src="imagens/site_flex.png" alt="Site FlexBox"></a>
                             <p>Site feito com html, css, JavaScript, PHP</p>
-                            </center>
                         </li>
                     </ul>
                 </div>
-                <div class="modulo verde">
+                <div class="modulo treze">
+                    <h3>Site de Hotel</h3>
+                    <ul>
+                        <li>
+                            <p>Hotel Paraiso</p>
+                            <a href="/index.php">
+                            <img src="imagens/site_flex.png" alt="Site FlexBox"></a>
+                            <p>Site feito com html, css, JavaScript, PHP</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo doze">
                     <h3>Split Landing PageSite </h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Play and Xbox animado com css e js</p>
                             <a href="ProjectDaySeven/index.html">
                             <img src="imagens/mauseenter.png" alt="Site Com Animação mause enter"></a>
                             <p>Site feito com html, css, JavaScript, PHP</p>
-                            </center>
                         </li>
                     </ul>
                 </div>
-                <div class="modulo violeta">
+                <div class="modulo onze">
                     <h3>FAQ </h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Frequenly Ask</p>
                             <a href="ProjectDayTwelve/index.html">
                             <img src="imagens/faq.png" alt="Site Com Animação mause enter"></a>
                             <p>Site feito com html, css, JavaScript, PHP</p>
-                            </center>
                         </li>
                     </ul>
                 </div>
-                <div class="modulo laranja-dark">
+                <div class="modulo dez">
                     <h3>Key Code </h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Key code</p>
                             <a href="ProjectDayEleven/index.html">
                             <img src="imagens/key.png" alt="Site Com Animação mause enter"></a>
                             <p>Site feito com html, css, JavaScript, PHP</p>
-                            </center>
                         </li>
                     </ul>
                 </div>
-                <div class="modulo gradiente">
+                <div class="modulo nove">
                     <h3>Sound Board </h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Sound Board Click</p>
                             <a href="ProjectDayNine/index.html">
                             <img src="imagens/sound.png" alt="Site Com Animação mause enter"></a>
                             <p>Site feito com html, css, JavaScript, PHP</p>
-                            </center>
                         </li>
                     </ul>
                 </div>
-                <div class="modulo rosa-claro">
+                <div class="modulo oito">
                     <h3>Input Wave</h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Input animated</p>
                             <a href="ProjectDayEight/index.html">
                             <img src="imagens/waveinp.png" alt="Site Com Animação mause enter"></a>
                             <p>Site feito com html, css, JavaScript, PHP</p>
-                            </center>
                         </li>
                     </ul>
                 </div>
-                <div class="modulo rosa-escandalo">
+                <div class="modulo sete">
                     <h3>Joke api</h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Joke</p>
                             <a href="ProjectDayTen/index.html">
                             <img src="imagens/joke.png" alt="Site Com Animação mause enter"></a>
                             <p>Site feito com html, css, JavaScript, PHP</p>
-                            </center>
                         </li>
                     </ul>
                 </div>
-                <div class="modulo roxo">
+                <div class="modulo seis">
                     <h3>Estações</h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Fotos Estações</p>
                             <a href="projectDayOne/index.html">
                             <img src="imagens/sum.png" alt="Sites Js"></a>
                             <p>Site feito com html, css, JavaScript</p>
-                            </center>
                         </li>
-
                     </ul>
                 </div>
-                <div class="modulo azul">
+                <div class="modulo cinco">
                     <h3>Etapa Dinâmica</h3>
                     <ul>
                         <li>
-                            <center>
                             <p>ETAPAS DE PROGRESSO</p>
                             <a href="projectDayTwo/index.html">
                             <img src="imagens/steps.png" alt="Sites Js"></a>
                             <p>Site feito com html, css, JavaScript</p>
-                            </center>
+                            
                         </li>
-
                     </ul>
                 </div>
-                <div class="modulo verde-escuro">
+                <div class="modulo quatro">
                     <h3>Amazing Article</h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Site com elevação ao ativar o menu</p>
                             <a href="ProjectDayThree/index.html">
                             <img src="imagens/amazing.png" alt="Sites Js"></a>
                             <p>Site feito com html, css, JavaScript</p>
-                            </center>
                         </li>
-
                     </ul>
                 </div>
-                <div class="modulo roxo-escuro">
+                <div class="modulo tres">
                     <h3>Search Dinâmico</h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Search com transition</p>
                             <a href="ProjectDayFour/index.html">
                             <img src="imagens/search.png" alt="Sites Js"></a>
                             <p>Site feito com html, css, JavaScript</p>
-                            </center>
                         </li>
-
                     </ul>
                 </div>
-                <div class="modulo vermelho">
+                <div class="modulo dois">
                     <h3>Blur animate</h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Blur</p>
                             <a href="ProjectDayFive/index.html">
                             <img src="imagens/blur.png" alt="Sites Js"></a>
                             <p>Site feito com html, css, JavaScript</p>
-                            </center>
+                            
                         </li>
-
                     </ul>
                 </div>
-                <div class="modulo azul-escuro">
+                <div class="modulo um">
                     <h3>Scroll to see the animation</h3>
                     <ul>
                         <li>
-                            <center>
                             <p>Scroll</p>
                             <a href="ProjectDaySix/index.html">
                             <img src="imagens/scroll.png" alt="Site Scroll Js"></a>
                             <p>Site feito com html, css, JavaScript</p>
-                            </center>
+                            
                         </li>
-
                     </ul>
                 </div>
             </nav>
